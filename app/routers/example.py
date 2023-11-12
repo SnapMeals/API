6f21801389c5f909d02c5c7a2af541ed7d520f94
itsbriany <itsbriany@gmail.com>
2023-11-11 23:29:28 -0400

    Adding example - still broken with no Personal org found.

--- app/routers/example.py
+++ app/routers/example.py
@@ -1,7 +1,21 @@
+import os
+import openai
 from fastapi import APIRouter
+from openai import OpenAI
 
 router = APIRouter()
 
+client = OpenAI()
+
 @router.get("/example")
 def read_example():
-    return {"example": "response"}
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    return {"example": response}
